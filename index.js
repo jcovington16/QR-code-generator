94127422510aad05ff31c3116e205085fbea73eb
jcovington16 <covingtonjoshua7@gmail.com>
2021-09-23 17:14:46 -0600

    able to authenticate and run the discord bot. need to be able to generate some text and receive back a qr code.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const {Client, Intents} = require('discord.js');
+const {Client, Intents, Discord} = require('discord.js');
 require('dotenv').config();
 
 // const user_prompt = require('prompt-sync')();
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-console.log('Beginning');
 
 client.once('ready', () => {
     console.log('Discord Bot is running...')
@@ -14,7 +13,41 @@ client.on('message', (message) => {
     if (client.user.id === message.author.id) {
         return;
     }
-})
+
+    const userName = `@${message.author.id}`
+    const args = message.content.split(' ');
+    const argsExt = args.splice(1);
+
+    let url = `https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=${argsExt.join('+')}`;
+
+    if(args[0] === '~generate') {
+        if(argsExt.length === 0) {
+            message.channel.send(`${userName} - ERROR: NOTHING PROVIDED`);
+            return;
+        }
+       const qrCode = new Discord.MessageEmbed()
+            .setTitle('QR Code Generated')
+            .addFields(
+                {
+                    name: 'Requestor',
+                    value: userName
+                },
+                {
+                    name: 'Encoded Text',
+                    value: argsExt.join(' ')
+                },
+                {
+                    name: 'URL',
+                    value: url
+                }
+            )
+            .setImage(url)
+            .setTimestamp();
+
+        message.channel.send(qrCode)
+        console.log(message)
+    }
+});
 
 
 client.login(process.env.BOT_TOKEN)
